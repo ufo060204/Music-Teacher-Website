047b2667261f8d7ee3f3efbfba85dca3fd41a62d
ufo060204 <ufo060204@gmail.com>
2023-10-05 14:25:01 +0800

    新增 購買紀錄 RWD

--- src/views/front/PersonalOrders.vue
+++ src/views/front/PersonalOrders.vue
@@ -3,11 +3,11 @@
     <div class="container">
       <BannerArea />
       <ListPlaceholderVue v-if="isLoading" class="my-32"/>
-      <div v-if="!isLoading && !isBuyHistory" class="d-flex h-custom flex-column justify-content-center align-items-center">
+      <div v-if="!isLoading && !isBuyHistory" class="d-flex flex-column justify-content-center align-items-center my-32">
         <p class="fs-1 my-16">您沒有購買紀錄</p>
         <router-link to="/courses" class="btn btn-primary rounded-4">瀏覽課程</router-link>
       </div>
-      <div v-if="!isLoading && isBuyHistory" class="card rounded-4 shadow-sm my-32">
+      <div v-if="!isLoading && isBuyHistory" class="card rounded-4 shadow-sm my-32 d-none d-md-block">
         <div class="card-body">
           <div class="table-responsive">
             <table class="table table-hover">
@@ -35,6 +35,31 @@
                 </template>
               </tbody>
             </table>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="!isLoading && isBuyHistory" class="accordion my-32 d-md-none" id="accordionOrder">
+        <div v-for="(order, index) in myOrders" :key="order.orderId" class="accordion-item">
+          <h2 class="accordion-header" :id="`headingOne` + index">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" :data-bs-target="`#collapse` + index" aria-expanded="true" :aria-controls="`#collapse` + index">
+              <div class="d-flex justify-content-between w-100">
+                <p>
+                  {{ this.$moment(order.createdTime).format('YYYY年M月D日') }}
+                </p>
+                <p class="me-32">
+                  <span v-if="order.useCoupon" class="text-nowrap">NT$ {{ order.couponTotal }}</span>
+                  <span v-if="!order.useCoupon" class="text-nowrap">NT$ {{ order.total }}</span>
+                </p>
+              </div>
+            </button>
+          </h2>
+          <div :id="'collapse' + index" class="accordion-collapse collapse" :aria-labelledby="`headingOne` + index">
+            <div class="accordion-body">
+              <p class="fw-bold" v-for="(course) in order.courseDetails" :key="course.courseId">
+                {{ course.name }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -77,7 +102,4 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.h-custom {
-  height: calc(100vh - 410px);
-}
 </style>
